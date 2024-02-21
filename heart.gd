26e0f16584c3a65b4418d56f11058fe4004cc889
jadeng3 <jadeng3@s.sfusd.edu>
2024-02-21 12:55:18 -0800

    finished progress check #6

--- heart.gd
+++ heart.gd
@@ -2,5 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	queue_free()
-	var hears = get_tree().get_nodes_in_group("Hearts")
-	print(hearts.size())
+	var hearts = get_tree().get_nodes_in_group("Hearts")
+	if hearts.size() == 1:
+		Events.level_completed.emit()
+		print("Level Completed")
